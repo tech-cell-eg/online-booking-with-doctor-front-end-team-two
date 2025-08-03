deb57e240dcc9e35344fdaf36c0755f378c2901f
seif <mohamedahmedseifofficial@gmail.com>
2025-08-03 21:30:24 +0300

    Final Config

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import './App.css'
+import { Button } from './components/ui/button'
 function App() {
   return (
     <>
       <h1 className='bg-red-800'>Hello</h1>
+      <Button>Text</Button>
     </>
   )
 }
