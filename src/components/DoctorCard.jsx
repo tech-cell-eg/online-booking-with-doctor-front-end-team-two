36fb4f8d348e961997656592bf0ea2c03259d11e
seif <mohamedahmedseifofficial@gmail.com>
2025-08-04 18:20:34 +0300

    continue developing doctor-card component

--- src/components/DoctorCard.jsx
+++ src/components/DoctorCard.jsx
@@ -1,44 +1,53 @@
 import { Clock, Heart, Star } from "lucide-react";
-import { Card, CardContent, CardTitle } from "./ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
 import { useState } from "react";
 
 function DoctorCard({ doctor }) {
-  const { id, name, specialty, hospital, rating, totalRatings, workingHours } = doctor;
+  const { id, name, specialty, hospital, rating, totalRatings, workingHours } =
+    doctor;
   const [isLike, setIsLike] = useState(false);
   return (
     <>
-      <Card className="w-full max-w-[450px] max-h-[100px] flex border-[#BBC1C7]">
-        <div className="basis-1/4">
-          <img
-            src="/public/doctor.jpg"
-            alt="doctor"
-            className="w-full h-full object-cover object-top rounded-l-xl"
-          />
+      <Card className="doctor-card">
+        {/* Image */}
+        <div className="basis-1/4 h-full">
+          <img src="/public/doctor.jpg" alt="doctor" className="doctor-image" />
         </div>
-        <div className="flex flex-1 items-center justify-between py-1">
-          <CardContent className="flex h-full flex-col justify-around p-0 pl-4 items-start">
-            <CardTitle className="text-lg font-news">{name}</CardTitle>
-            <p className="text-gray-600">{specialty} | {hospital}</p>
+
+        {/* Content */}
+        <div className="flex-1 pl-4">
+          <CardHeader className="p-0">
+            <CardTitle className="doctor-title">{name}</CardTitle>
+          </CardHeader>
+          <CardContent className="doctor-content">
+            <p className="doctor-description">
+              {specialty} | {hospital}
+            </p>
             <div className="flex gap-6 pt-2">
-              <div className="flex items-center gap-2">
+              <div className="flex-group">
                 <Star size={20} fill="gold" stroke="0" />
-                <span className="font-semibold">{rating}</span>
+                <p className="doctor-footer-text">{rating}</p>
               </div>
-              <div className="flex items-center gap-2">
-                <Clock size={20} />
-                <p className="font-semibold text-indigo-950">{workingHours.start} - {workingHours.end}</p>
+              <div className="flex-group">
+                <Clock size={20} color="#7d848a" strokeWidth={1.5}/>
+                <p className="doctor-footer-text">
+                  {workingHours.start} - {workingHours.end}
+                </p>
               </div>
             </div>
           </CardContent>
-          <div className="pr-4">
-            <Heart
-              className="cursor-pointer hover:fill-[#ff0000] hover:stroke-none transition-all duration-300"
-              size={25}
-              fill={`${isLike ? "red" : "white"}`}
-              onClick={() => setIsLike((prev) => !prev)}
-              stroke={`${isLike ? "none" : "black"}`}
-            />
-          </div>
+        </div>
+
+        {/* Like */}
+        <div className="pr-4">
+          <Heart
+            className="cursor-pointer hover:fill-[#ff0000] hover:stroke-none transition-all duration-300"
+            size={25}
+            fill={`${isLike ? "red" : "white"}`}
+            onClick={() => setIsLike((prev) => !prev)}
+            stroke={`${isLike ? "none" : "black"}`}
+            strokeWidth={0.8}
+          />
         </div>
       </Card>
     </>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,8 +2,9 @@ import DoctorCard from "@/components/DoctorCard";
 import { doctors } from "../../Data/doctors.json";
 
 function Home() {
+  const doctor = doctors[0]
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center ">
       <h1 className="text-xl font-bold">Home Page</h1>
       <div className="flex flex-col items-center gap-6">
         {doctors.map((doctor) => {
